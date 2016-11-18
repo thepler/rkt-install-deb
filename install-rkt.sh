fbaaba10c121d0c27f838a3a768f1f1e1ad960f2
Todd Hepler <todd.hepler@ctl.io>
2016-11-18 20:47:29 +0000

    here's the code

---
+++ install-rkt.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# taken from:
+# https://github.com/coreos/rkt/blob/master/Documentation/distributions.md#deb-based
+
+set -x
+
+gpg --recv-key 18AD5014C99EF7E3BA5F6CE950BDD3E0FC8A365E
+wget https://github.com/coreos/rkt/releases/download/v1.19.0/rkt_1.19.0-1_amd64.deb
+wget https://github.com/coreos/rkt/releases/download/v1.19.0/rkt_1.19.0-1_amd64.deb.asc
+gpg --verify rkt_1.19.0-1_amd64.deb.asc
+sudo dpkg -i rkt_1.19.0-1_amd64.deb
+
