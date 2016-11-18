97f8e41c8567ae667fa86e4f56c61540e2696808
Todd Hepler <todd.hepler@ctl.io>
2016-11-18 21:09:20 +0000

    set -e

--- install-rkt.sh
+++ install-rkt.sh
@@ -4,6 +4,7 @@
 # https://github.com/coreos/rkt/blob/master/Documentation/distributions.md#deb-based
 
 set -x
+set -e
 
 gpg --recv-key 18AD5014C99EF7E3BA5F6CE950BDD3E0FC8A365E
 wget https://github.com/coreos/rkt/releases/download/v1.19.0/rkt_1.19.0-1_amd64.deb
